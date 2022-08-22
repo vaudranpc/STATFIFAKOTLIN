1a7a00c4a81a0e3e8211e6d7e159851cd23c7123
anhe225 <anhe-pascal.anhe.1@etsmtl.net>
2022-08-22 06:40:50 -0400

    AJOUT DE BOUTON

---
+++ app/src/main/java/com/vaudran225/statfifakotlin/StatistiqueActitivty.kt
@@ -0,0 +1,11 @@
+package com.vaudran225.statfifakotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StatistiqueActitivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistique_actitivty)
+    }
+}
